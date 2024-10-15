c65f8deeb03309a205d56d11e885b9a69bccd1d7
최재혁 <dydkwogur@naver.com>
2024-10-16 00:27:08 +0900

    fisrt commit

--- .사용자정의함수3.cpp
+++ .사용자정의함수3.cpp
@@ -3,5 +3,16 @@
 int getDays(int month, int day)
 {
 	int i, sum = 0;
-	for(i = 1; i < month, i++)
+	for (i = 1; i < month, i++)
+	{
+		if (i == 2) //이 프로그램에서는 윤년을 계산하지 않습니다.
+		{
+			sum += 28;
+		}
+		else if (i % 2 == 0)
+		{
+			sum += 30;
+		}
+
+	}
 }
