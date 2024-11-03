5e8f60751a91609fb3a3efd946fea52ce4abec0e
최재혁 <dydkwogur@naver.com>
2024-11-03 17:42:30 +0900

    first comit

--- 배열1.cpp
+++ 배열1.cpp
@@ -5,5 +5,15 @@ int main(void)
 {
 	int i, max, min, index;
 	int array[number];
+	for (i = 0; i < NUMBER; i++)
+	{
+		scanf("%d", &array[i]);
+		if (max < array[i])
+		{
+			max = array[i];
+			index = i;
+		}
+	}
+	printf("가장 큰 값은 %d입니다. 그리고 %d번째에 있습니다.\n", max, index + 1);
 	return 0;
 }
