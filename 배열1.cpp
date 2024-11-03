8ab110a88040651eb8a732716f0b110e88e1afb0
최재혁 <dydkwogur@naver.com>
2024-11-04 00:50:14 +0900

    first commit

--- 배열1.cpp
+++ 배열1.cpp
@@ -15,5 +15,14 @@ int main(void)
 		}
 	}
 	printf("가장 큰 값은 %d입니다. 그리고 %d번째에 있습니다.\n", max, index + 1);
+	min = INT_MAX;
+	for (i = 0; i < NUMBER; i++)
+	{
+		scanf("%d", &array[i]);
+		if (min > array[i])
+		{
+
+		}
+	}
 	return 0;
 }
