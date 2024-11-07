3f5c22c431fbd606c527197f7036c4b4456e98cc
최재혁 <dydkwogur@naver.com>
2024-11-08 08:02:36 +0900

    fisrt commit

---
+++ 배열2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define NUMBER 5
+
+int main(void)
+{
+	int array[NUMBER];
+	int i, oddMax, evenMax;
+	oddMax = 0;
+	evenMax = 0;
+	for (i = 0; i < NUMBER; i++)
+	{
+		scanf()
+	}
+}
